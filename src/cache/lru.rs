2fefc572a46341f453d921fef53183a4ea7aa5fb
Kipkoech854 <gideonkipkoech854@gmail.com>
2026-07-03 13:35:49 +0300

    commit with a first iteration working set,add_to_front,move_to_front and evict_tail methods

--- src/cache/lru.rs
+++ src/cache/lru.rs
@@ -1,22 +1,22 @@
+//use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::error::Error;
-
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::sync::{Arc, RwLock};
 
 #[derive(Debug, Clone)]
 pub struct Node {
-    key: Box<String>,
+    key: String,
     value: String,
-    prev: Option<Rc<RefCell<Node>>>,
+    prev: Option<Weak<RefCell<Node>>>,
     next: Option<Rc<RefCell<Node>>>,
 }
 
 impl Node {
     pub fn new(key: String, value: String) -> Self {
         Node {
-            key: Box::new(key),
+            key: key,
             value: value,
             prev: None,
             next: None,
@@ -24,105 +24,156 @@ impl Node {
     }
 }
 
+#[derive(Debug)]
 pub struct Cache {
-    head: Rc<RefCell<Node>>,
-    tail: Rc<RefCell<Node>>,
+    head: Option<Rc<RefCell<Node>>>,
+    tail: Option<Rc<RefCell<Node>>>,
     map: HashMap<String, Rc<RefCell<Node>>>,
     capacity: usize,
     size: usize,
 }
 
 impl Cache {
-    pub fn new(node: &Node, capacity: usize) -> Self {
-        let ptr = Rc::new(RefCell::new(node.clone()));
+    pub fn new(key: String, value: String, capacity: usize) -> Self {
+        //create new node
+        let node = Node::new(key.clone(), value);
+
+        // Wrap it in a local smart pointer
+        let ptr = Rc::new(RefCell::new(node));
         let mut hash = HashMap::new();
-        hash.insert(*node.key.clone(), Rc::clone(&ptr));
+        hash.insert(key, Rc::clone(&ptr));
         Cache {
-            head: Rc::clone(&ptr),
-            tail: Rc::clone(&ptr),
+            head: Some(Rc::clone(&ptr)),
+            tail: Some(Rc::clone(&ptr)),
             map: hash,
             capacity: capacity,
             size: 1,
         }
     }
 
-    pub fn set(cache: Arc<RwLock<Cache>>, node: Rc<RefCell<Node>>) -> Result<(), dyn Error> {
-        //the thread tries to acquire the lock  write() is blocking and will cause the thread to wait for the lock
-        // [TODO]  implement try_write to avoid blocking in case of many request
+    pub fn set(cache: Arc<RwLock<Cache>>, node: Rc<RefCell<Node>>) -> Result<(), Box<dyn Error>> {
         let mut set_grd = cache.write().unwrap();
-        //[TODO] call move to front instead of  a new set if it exists
-        if (*set_grd).map.contains_key(&node.key) {
+
+        // Read the key early (This is read-only and safe)
+        let node_key = node.borrow().key.clone();
+        //  Clone the node pointer for the map BEFORE moving the original into add_to_front later
+        let map_node_pointer = Rc::clone(&node);
+
+        // Check if it exists
+        if set_grd.map.contains_key(&node_key) {
+            //[TODO] use a signal for move to front
+            Self::move_to_front(Arc::clone(&cache), map_node_pointer)?;
             return Ok(());
         }
-        //[TODO] check the size of cache against capacity to alert evict tail if capacity is close to full
-        //if the key doesnt exist add to front
-        Self::add_to_front(node, set_grd).unwrap();
+
+        // [TODO] Check capacity and evict_tail here before adding new items
+
+        // Run the list manipulation. If this panics, the cache state below isn't mutated.
+        Self::add_to_front(node, &mut *set_grd)?;
+
+        // safely commit changes to the cache state
+        set_grd.map.insert(node_key, map_node_pointer);
+        set_grd.size += 1;
+
         Ok(())
     }
 
     pub fn add_to_front(
         node: Rc<RefCell<Node>>,
-        write_grd: RwLockWriteGuard<'_, Cache>,
-    ) -> Result<(), dyn Error> {
-        //extract details like the cache's head, tail and capaity that will be used in adding ro front
-        let (exhead, capacity) = ((write_grd).head, (write_grd).capacity);
-        //access the new node being added to front to mutate the next pointer to exhead and prev pointer to the node being added
-        let adding_node = node.try_borrow_mut().unwrap();
-        (adding_node.next, adding_node.prev) = (exhead, Some(node));
+        cache: &mut Cache, // Accept a mutable reference to the Cache instead of consuming the guard
+    ) -> Result<(), Box<dyn Error>> {
+        // 1. Take the current head out of the cache cleanly.
+        // .take() leaves `None` in the cache temporarily and gives us ownership of the old head.
+        let exhead = cache.head.take();
 
-        //change the head ptr
-        write_grd.head = node;
-        // insert the new nodes key and the node the hashmap already in cache
-        write_grd.map.insert(*adding_node.key, node);
-        //add 1 to the size
-        // [TODO] update size to actually use bytes
-        write_grd.size += 1;
+        // 2. Configure the new head node's pointers
+        {
+            let mut adding_node = node.borrow_mut();
+
+            // Next points to the old head (if it existed)
+            adding_node.next = exhead.clone();
+
+            // Head node's PREV must ALWAYS be None!
+            adding_node.prev = None;
+        } // Borrow drops here safely
+
+        // 3. If there was an old head, update its `prev` pointer to point back to our new node
+        if let Some(old_head_rc) = exhead {
+            old_head_rc.borrow_mut().prev = Some(Rc::downgrade(&node));
+        } else {
+            // If there was NO old head, it means the cache was empty!
+            // Therefore, this new node is also the TAIL.
+            cache.tail = Some(Rc::clone(&node));
+        }
+
+        // Set the cache head to our new node
+        cache.head = Some(node);
 
         Ok(())
     }
+
     pub fn move_to_front(
         cache: Arc<RwLock<Cache>>,
         node: Rc<RefCell<Node>>,
-    ) -> Result<(), dyn Error> {
+    ) -> Result<(), Box<dyn Error>> {
         //the thread tries to acquire the lock  write() is blocking and will cause the thread to wait for the lock
         // [TODO]  implement try_write to avoid blocking in case of many request
+        // 1. Acquire write lock
         let mut set_grd = cache.write().unwrap();
-        //check if the node we want to move is the head and if it is return the cache unchanged
-        if set_grd.head == node {
+
+        // 2. Check if it's already the head (Using the Option comparison we discussed!)
+        /*if set_grd.head.as_ref() == Some(&node) {
             println!("The node is already head");
             return Ok(());
+        }*/
+
+        // 3. SAFELY extract neighbor pointers without holding borrows open
+        // We clone the Options (cheap 8-byte pointer increments) so we can drop the borrow instantly.
+        let (prev_opt, next_opt) = {
+            let borrowed = node.borrow();
+            (borrowed.prev.clone(), borrowed.next.clone())
+        }; // The borrow on `node` drops right here! Safe from deadlocks.
+
+        // 4. Stitch the neighbors together (Bypass the moving node)
+        match (prev_opt, next_opt) {
+            (None, Some(next)) => {
+                // Case C: Node is the HEAD (Handled at the top, but keeping logic sound)
+                next.borrow_mut().prev = None;
+                set_grd.head = Some(next);
+            }
+            (Some(prev), Some(next)) => {
+                // Case A: Node is in the middle of the list
+                if let Some(prev_rc) = prev.upgrade() {
+                    prev_rc.borrow_mut().next = Some(Rc::clone(&next));
+                    next.borrow_mut().prev = Some(Rc::downgrade(&prev_rc));
+                }
+            }
+            (Some(prev), None) => {
+                // Case B: Node is the TAIL (No next node)
+                if let Some(prev_rc) = prev.upgrade() {
+                    prev_rc.borrow_mut().next = None;
+                    set_grd.tail = Some(prev_rc); // The previous node becomes the new tail
+                }
+            }
+
+            (None, None) => {
+                // Case D: It's the only item in the list, do nothing
+            }
         }
-        ////access the new node being added to front to mutate the next pointer to exhead and prev pointer to the node being added
-        let moving_node = node.try_borrow_mut().unwrap();
-        //get the previos node to the moving node && the next node
-        let moving_prev_node = moving_node.prev.try_borrow_mut().unwrap();
-        let moving_next_node = moving_node.next.try_borrow_mut().unwrap();
-        // check if the node being moved is the tail to avoid seting an invalid next node
-        if set_grd.tail == node {
-            //set the prev's next node to None
-            moving_prev_node.next = None;
-            //set prev node to be the new tail
-            set_grd.tail =
-                moving_prev_node.expect("Error setting new tail while moving to the front");
-            //call the function to move it to the front
-            let moved = Self::add_to_front(node, set_grd).unwrap();
-            return Ok(());
+
+        // 5. Now that the node is completely detached from its old position,
+        // clear its old pointers before sending it to the front.
+        {
+            let mut borrowed = node.borrow_mut();
+            borrowed.prev = None;
+            borrowed.next = None;
         }
-        //This applies to when the node being moved is not the head nor the tail
-        //Look for the previous node to th node being moved
-        //change the pointer of its next node to point to the next of the node being moved
-        if let Some(prev_node) = moving_prev_node {
-            (prev_node).next = moving_node.next;
-        }
-        //look for the next node to the node being moved
-        //Change the pointer of its previous node to the previous of the node being moved
-        if let Some(next_node) = moving_next_node {
-            next_node.prev = moving_node.prev;
-        }
-        //call the function add_to_front to move the node to the front as new head
-        let moved = Self::add_to_front(node, set_grd);
-        //return the new cache with moved values
-        Ok(())
+
+        // 6. Push to front
+        // Ensure your add_to_front accepts a &mut Cache reference or uses set_grd
+        let result = Self::add_to_front(node, &mut *set_grd).unwrap();
+
+        Ok(result)
     }
     pub fn evict_tail(cache: Arc<RwLock<Cache>>) -> Result<(), Box<dyn Error>> {
         // Attempt to get the write lock non-blocking
@@ -143,12 +194,12 @@ impl Cache {
             // when modifying pointers next!
             drop(old_tail_borrow);
 
-            if let Some(prev_rc) = prev_node_option {
-                // Cut the link: Set the previous node's 'next' pointer to None
-                prev_rc.borrow_mut().next = None;
-
-                // Update the cache's tail to be this previous node
-                set_grd.tail = prev_rc;
+            if let Some(prev_weak) = prev_node_option {
+                // Upgrade the weak pointer to an Rc to use it
+                if let Some(prev_rc) = prev_weak.upgrade() {
+                    prev_rc.borrow_mut().next = None;
+                    set_grd.tail = Some(prev_rc);
+                }
             }
 
             // This drops the HashMap's strong Rc reference to the node
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 mod cache;
 
-fn main(){
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::{Arc, RwLock};
+
+fn main() {
     println!("Hello world");
-    let new_node = cache::lru::Node::new(String::from("This is te key"), String::from("This is the value"));
-    println!("made a new node:{:?}", new_node);
+
+    let new_node = cache::lru::Cache::new(
+        String::from("This should be tail"),
+        String::from("This is the value of tail"),
+        10,
+    );
+    let new_node1 = cache::lru::Node::new(
+        String::from("This should be head"),
+        String::from("This is the value of head"),
+    );
+    let new_rc = Rc::new(RefCell::new(new_node1));
+    let new_lock = Arc::new(RwLock::new(new_node));
+    cache::lru::Cache::set(Arc::clone(&new_lock), new_rc).unwrap();
+    // Instead of printing `new_lock` directly, print its read guard!
+    println!("made a new node:{:?}", new_lock.read().unwrap());
 }
